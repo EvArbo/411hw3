a10146f63c0b5b2aab64819726515eeaad24e6ae
ycl0408 <ycl0408@bu.edu>
2024-11-04 21:24:08 -0500

    Update test_battle_model.py - 2

--- meal_max/tests/test_battle_model.py
+++ meal_max/tests/test_battle_model.py
@@ -5,7 +5,7 @@ from meal_max.models.kitchen_model import Meal
 
 @pytest.fixture()
 def battle_model():
-    """Fixture to provide a new instance of PlaylistModel for each test."""
+    """Fixture to provide a new instance of BattleModel for each test."""
     return BattleModel()
 
 @pytest.fixture
@@ -24,6 +24,46 @@ def sample_meal2():
 @pytest.fixture
 def sample_battle(sample_meal1, sample_meal2):
     return [sample_meal1, sample_meal2]
+
+##################################################
+# Battle Outcome Test Cases
+##################################################
+
+def test_battle_normal_outcome(battle_model, sample_battle):
+    """Test the normal outcome of a battle"""
+    battle_model.combatants = (sample_battle)
+    
+    mocker.patch.object(battle_model, 'get_battle_score', side_effect=[50, 30])
+    mocker.patch('battle_model.get_random', return_value=0.1)
+
+    assert battle_model.battle() == "Meal 1", "Meal 1 is the expected winner"
+    
+    mock_update_stats.assert_any_call(1, 'win')
+    mock_update_stats.assert_any_call(2, 'loss')
+    
+    assert len(battle_model.combatants) == 1
+    assert battle_model.combatants[0] == sample_meal1, "Loser was not removed correctly."
+
+def test_battle_random_outcome(battle_model, sample_battle):
+    """Test the normal outcome of a battle"""
+    battle_model.combatants = (sample_battle)
+    
+    mocker.patch.object(battle_model, 'get_battle_score', side_effect=[30, 32])
+    mocker.patch('battle_model.get_random', return_value=0.025)
+
+    assert battle_model.battle() == "Meal 2", "Meal 2 is the expected winner"
+    
+    mock_update_stats.assert_any_call(2, 'win')
+    mock_update_stats.assert_any_call(1, 'loss')
+    
+    assert len(battle_model.combatants) == 1
+    assert battle_model.combatants[0] == sample_meal2, "Loser was not removed correctly."
+
+def test_battle_not_enough_combatants():
+    """Test error when intiating battle with less than 2 combatants"""
+    battle_model.combatants = [Mock()]
+    with pytest.raises(ValueError, match="Two combatants must be prepped for a battle."):
+        battle_model.battle()
 
 ##################################################
 # Add Combatant Mangement Test Cases
@@ -52,8 +92,27 @@ def test_clear_battle(battle_model, sample_meal1):
     battle_model.clear_combatants()
     assert len(battle_model.combatants) == 0, "Battle should be empty after clearing"
 
-def test_clear_playlist_empty_playlist(battle_model, caplog):
-    """Test clearing the entire playlist when it's empty."""
-    playlist_model.clear_playlist()
-    assert len(playlist_model.playlist) == 0, "Playlist should be empty after clearing"
-    assert "Clearing an empty playlist" in caplog.text, "Expected warning message when clearing an empty playlist"
+def test_clear_battle_empty_battle(battle_model, caplog):
+    """Test clearing the battle when it's empty."""
+    battle_model.clear_combatants()
+    assert len(battle_model.combatants) == 0, "Battle should be empty after clearing"
+    assert "Clearing an empty battle" in caplog.text, "Expected warning message when clearing an empty battle"
+
+##################################################
+# Combatant Retrieval Test Cases
+##################################################
+
+def test_get_battlescore(battle_model, sample_meal1):
+    """Test successfully retrieving the battle score."""
+    score = battle_model.get_battle_score(sample_meal1)
+    expected_score = (10.0 * 9) - 1
+    assert score == expected_score, "The battle score retrieved must be equal to the calculated score"
+
+def test_get_all_songs(battle_model, sample_battle):
+    """Test successfully retrieving all combatants in the battle."""
+    battle_model.combatants.extend(sample_battle)
+
+    all_combatants = battle_model.get_combatants()
+    assert len(all_combatants) == 2
+    assert all_combatants[0].id == 1
+    assert all_combatants[1].id == 2
